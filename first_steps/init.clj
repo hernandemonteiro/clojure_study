8d5ca63a12a057c2946cfb8972de51974aaaba92
Hernande Monteiro <monteiro1998@live.com>
2023-06-12 19:30:06 -0300

    start learn

--- first_steps/init.clj
+++ first_steps/init.clj
@@ -1,7 +1,29 @@
-;; imprimindo na tela
 (println "Começando usar clojure!")
 
-;; declaração de variavel
+
+;; ###############################
+
+
+;; isso é um comentário de linha
+
+(comment
+Isso é um comentário multilinhas
+)
+
+
+;; ###############################
+
+
+;; declaração de variavel não constantes
 (def total-na-variavel 15)
 
 (println "Variável:"total-na-variavel)
+
+
+;; ###############################
+
+
+;; incrementando variavel
+(def total-na-variavel (+ total-na-variavel 3))
+
+(println total-na-variavel)
